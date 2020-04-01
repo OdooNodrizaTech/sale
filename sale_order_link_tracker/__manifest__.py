422db8b21e717e4106551f15e285aea9e797e839
Informatica Arelux <informatica@arelux.com>
2020-04-01 15:28:45 +0200

    upload addons

---
+++ sale_order_link_tracker/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sale Order Link Tracker',
+    'version': '10.0.1.0.0',    
+    'author': 'Odoo Nodriza Tech (ONT)',
+    'website': 'https://nodrizatech.com/',
+    'category': 'Tools',
+    'license': 'AGPL-3',
+    'depends': ['base', 'sale', 'link_tracker'],
+    'data': [
+        'data/ir_cron.xml',
+    ],
+    'installable': True,
+    'auto_install': False,    
+}
