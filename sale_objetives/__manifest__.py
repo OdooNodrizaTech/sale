22f99b27d5f39f6e52a87580e9bc4f1779d3ee02
Informatica Arelux <informatica@arelux.com>
2020-04-02 13:01:30 +0200

    sale_objetives

--- sale_objetives/__manifest__.py
+++ sale_objetives/__manifest__.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Sale objetives',
-    'version': '1.1.1',
-    'author': '@victor.almau',
-    'category': 'Sale',
-    'complexity': 'normal',
+    'name': 'Sale Objetives',
+    'version': '10.0.1.0.0',    
+    'author': 'Odoo Nodriza Tech (ONT)',
+    'website': 'https://nodrizatech.com/',
+    'category': 'Tools',
+    'license': 'AGPL-3',
     'depends': ['base', 'sale'],
     'data': [        
         'security/ir.model.access.csv',
         'views/sale_objetives.xml',
     ],
-    'tests': [],
     'installable': True,
-    'auto_install': False,
-    'license': 'AGPL-3',
- }
+    'auto_install': False,    
+}
