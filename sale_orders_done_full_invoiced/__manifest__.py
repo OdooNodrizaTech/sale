f6f5f303447427a808e923a4fefcd6bcd2f5121e
Informatica Arelux <informatica@arelux.com>
2020-04-06 09:08:15 +0200

    rename

--- sale_orders_done_full_invoiced/__manifest__.py
+++ sale_orders_done_full_invoiced/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Sale Order Done Full Invoiced',
+    'name': 'Sale Orders Done Full Invoiced',
     'version': '10.0.1.0.0',    
     'author': 'Odoo Nodriza Tech (ONT)',
     'website': 'https://nodrizatech.com/',
