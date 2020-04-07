d941d768007ef51ad5ddb06c494dc5c7b6cdd57b
Informatica Arelux <informatica@arelux.com>
2020-04-07 09:36:06 +0200

    fix

--- ont_base_sale/models/sale_order.py
+++ ont_base_sale/models/sale_order.py
@@ -12,13 +12,14 @@ class SaleOrder(models.Model):
     def action_confirm(self):
         allow_confirm = True
         #check
-        for obj in self:            
-            if obj.partner_id.vat==False:
-                allow_confirm = False
-                raise Warning("Es necesario definir VAT para el cliente antes de validar el pedido de venta.\n")
-            elif obj.partner_invoice_id.vat==False:
-                allow_confirm = False
-                raise Warning("Es necesario definir VAT para la direccion de facturacion antes de validar el pedido de venta.\n")                           
+        for obj in self:
+            if obj.amount_total>0:            
+                if obj.partner_id.vat==False:
+                    allow_confirm = False
+                    raise Warning("Es necesario definir VAT para el cliente antes de validar el pedido de venta.\n")
+                elif obj.partner_invoice_id.vat==False:
+                    allow_confirm = False
+                    raise Warning("Es necesario definir VAT para la direccion de facturacion antes de validar el pedido de venta.\n")                           
         #allow_confirm
         if allow_confirm==True:
             return super(SaleOrder, self).action_confirm()
