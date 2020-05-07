b0f88a82cce183e82125b31c185c950bdd9b41fa
Informatica Arelux <informatica@arelux.com>
2020-05-07 14:12:21 +0200

    v12 init
    cashondelivery

---
+++ cashondelivery/README.md
@@ -0,0 +1,6 @@
+Implementa el apartado de total_contrareembolso en los presupuestos (sale.order) y albaranes (stock.picking).
+
+## Parámetros de configuración
+```
+account_payment_mode_id_cashondelivery
+```
---
+++ cashondelivery/models/sale_order.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import logging
+_logger = logging.getLogger(__name__)
+
+from odoo import api, models, fields
+from odoo.exceptions import Warning
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+    
+    total_cashondelivery = fields.Float( 
+        string='Total contrareembolso'
+    )
+    
+    @api.multi
+    def action_confirm(self):
+        allow_confirm = True
+        #config
+        account_payment_mode_id_cashondelivery = int(self.env['ir.config_parameter'].sudo().get_param('account_payment_mode_id_cashondelivery'))
+        #check
+        for obj in self:
+            if obj.amount_total>0:                                        
+                if account_payment_mode_id_cashondelivery==self.payment_mode_id.id:
+                    if self.total_cashondelivery<10:
+                        allow_confirm = False
+                        raise Warning("No se puede confirmar la venta de contrareembolso con un total_contrareembolso menor de 10")                           
+        #allow_confirm
+        if allow_confirm==True:
+            return super(SaleOrder, self).action_confirm()
