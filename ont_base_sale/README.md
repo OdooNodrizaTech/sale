b0433d6b2a550e3a55aed073d71593dd60d7a6cf
victoralmau <victoralmau@gmail.com>
2020-07-14 07:43:30 +0200

    reestructuracion

--- ont_base_sale/README.md
+++
@@ -1,1 +0,0 @@
-Se coloca la limitación para que NO se pueda confirmar un presupuesto si el cliente NO tiene NIF.
--- sale_arelux/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-import res_partner
-import sale_order
