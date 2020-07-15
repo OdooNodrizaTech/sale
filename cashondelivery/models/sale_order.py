50f8e3d8738dae68b840ada4902e488991602c6d
victoralmau <victoralmau@gmail.com>
2020-07-15 07:52:47 +0200

    fix

--- cashondelivery/models/sale_order.py
+++ cashondelivery/models/sale_order.py
@@ -10,6 +10,17 @@ class SaleOrder(models.Model):
     total_cashondelivery = fields.Float(
         string='Total cashondelivery'
     )
+    payment_mode_id_is_cashondelivery = fields.Boolean(
+        string='Payment mode id is cashondelivery?',
+        compute='_compute_payment_mode_id_is_cashondelivery',
+        default=False
+    )
+
+    @api.multi
+    def _compute_payment_mode_id_is_cashondelivery(self):
+        self.ensure_one()
+        if self.payment_mode_id.id>0:
+            self.payment_mode_id_is_cashondelivery = self.payment_mode_id.is_cashondelivery
 
     @api.multi
     def action_confirm(self):
@@ -18,7 +29,7 @@ class SaleOrder(models.Model):
         for item in self:
             if item.amount_total>0:
                 if item.payment_mode_id.id>0:
-                    if item.payment_mode_id.is_cashondelivery==True:
+                    if item.payment_mode_id_is_cashondelivery==True:
                         if item.payment_mode_id.minimum_amount_cashondelivery>0 and item.total_cashondelivery<item.payment_mode_id.minimum_amount_cashondelivery:
                             allow_confirm = False
                             raise UserError(_('Cash on delivery cannot be confirmed with a cash on delivery total of less than %s') % (item.payment_mode_id.minimum_amount_cashondelivery))
